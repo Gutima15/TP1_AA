25aaf3b6ce722c81c177bd4bd54d13c2a399753d
josueggss73 <josueggss73@gmail.com>
2018-08-31 21:54:54 -0600

    Pruebas finales Euclidiana
    lo de arriba

--- AA_Van_Goh_Evolucional/src/logic/Experiment.java
+++ AA_Van_Goh_Evolucional/src/logic/Experiment.java
@@ -1,17 +1,28 @@
 package logic;
 
 import java.awt.Graphics2D;
+import java.awt.Image;
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 public class Experiment {
 
 	public static void main(String[] args) {
 		Factory factory = new Factory();
 		//factory.getRandomPicture(32,32,100,"C:\\Users\\josue\\Dropbox\\SEMESTRE 2 2018\\Analisis de Algoritmos\\Progra1\\Pictures\\");
-		BufferedImage image1 = factory.abrirImagen();
-		BufferedImage image2 = factory.abrirImagen();
-		Functions functions = new Functions();
-		System.out.println(functions.euclidianFunction(image1, image2));
+		try {
+			BufferedImage image1 = ImageIO.read(new File("C:\\Users\\josue\\Desktop\\negra2.jpg"));
+			BufferedImage image2 = ImageIO.read(new File("C:\\Users\\josue\\Desktop\\blanca2.jpg"));
+			Functions functions = new Functions();
+			System.out.println(functions.euclidianFunction(image1, image2));
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			System.out.println("ERROR");
+		}
 	}
 
 }
--- AA_Van_Goh_Evolucional/src/logic/Functions.java
+++ AA_Van_Goh_Evolucional/src/logic/Functions.java
@@ -3,6 +3,7 @@ package logic;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
+import java.text.DecimalFormat;
 
 public class Functions {
 
@@ -17,23 +18,27 @@ public class Functions {
 			for(int j = 0;j<image.getHeight();j++) {
 				Color colorP = new Color(image.getRGB(i, j));
 				Color colorQ = new Color(goalImage.getRGB(i, j));
-				double promP = (colorP.getBlue() + colorP.getGreen() + colorP.getRed())/3;
-				double promQ = (colorQ.getBlue() + colorQ.getGreen() + colorQ.getRed())/3;
 			    int RP = colorP.getRed();
 			    int BP = colorP.getBlue();
 				int GP = colorP.getGreen();
 				int RQ = colorQ.getRed();
 			    int BQ = colorQ.getBlue();
 				int GQ = colorQ.getGreen();
+				double promP = (RP + BP + GP)/3;
+				double promQ = (RQ + BQ + GQ)/3;
 				sum = sum + ((Math.pow(promP-promQ, 2))/(width*height));
 			    System.out.println("B:"+BP+" R:"+RP+" G:"+GP+"   B:"+BQ+" R:"+RQ+" G:"+GQ +" PromP: "+promP+" PromQ: "+promQ);
-			    /*
-			     * THIS IS A TEST, IT MIGHT WORK
-			     */
 			    
 			}	
 		}
 		System.out.println("x: "+width +" y: "+height);
-		return Math.sqrt(sum);
+		System.out.println("");
+		double square = Math.sqrt(sum);
+		double result = (((square*100)/255)-100);
+		
+		DecimalFormat df = new DecimalFormat("#.##");   
+		
+		return Math.abs(Double.valueOf(df.format(result)));
+		//return square;
 	}
 }
